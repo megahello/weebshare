e3d2853a76a50e221b1cd5b559abb4aca7ed6d13
bin senpai <theislamicgamer101@gmail.com>
2017-12-08 17:29:20 -0500

    file requester finsihed AND WOWIE MILME TYPES TOPKEK

--- baka/Controllers/MainController.cs
+++ baka/Controllers/MainController.cs
@@ -5,11 +5,47 @@ using System;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace baka.Controllers
 {
     public class MainController : BakaController
     {
-        
-    }   
+        [Route("/{*url}/{name}/.{ext?}/")]
+        [Route("/{*url}/{name}/.{ext?}")]
+        [Route("/{*url}/{name}/")]
+        [Route("/{*url}/{name}")]
+        [Route("/{name}/.{ext?}/")]
+        [Route("/{name}/.{ext?}")]
+        [Route("/{name}/")]
+        [Route("/{name}")]
+        public async Task<IActionResult> GetFileOrLink(string name, string ext, string url)
+        {
+            using (var context = new BakaContext())
+            {
+                var file = await context.Files.FirstOrDefaultAsync(u => u.ExternalId == name);
+
+                if (file == null)
+                {
+                    Response.StatusCode = 404;
+
+                    return Json(new
+                    {
+                        success = false,
+                        error = "404 Not Found",
+                        code = 404
+                    });
+                }
+                
+                string file_ext = Path.GetExtension(file.Filename);
+                string use_url_extention = Request.Query["u"].FirstOrDefault() ?? "0";
+
+                if(use_url_extention == "1" || use_url_extention == "true")
+                    file_ext = ext;
+
+                return File(await Globals.GetFile(file.ExternalId), BakaMime.GetMimeType(file_ext));
+            }
+        }
+    }
 }
