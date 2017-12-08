b6e265dfea0d43b2a44db7748806fcfa967f58c7
bin senpai <theislamicgamer101@gmail.com>
2017-12-07 19:30:09 -0500

    ok

--- baka/Globals.cs
+++ baka/Globals.cs
@@ -1,12 +1,17 @@
 using System;
 using baka.Models;
 
-namespace baka {
-    public static class Globals {
+namespace baka
+{
+    public static class Globals
+    {
         public const string ConfigFileName = "baka_config.json";
-        public static ConfigModel Config { get; set; }
-        public static void Initliaze () {
-            Config = ConfigModel.GetConfig (ConfigFileName);
+        public static ConfigModel Config { get; 
+        set; }
+
+        public static void Initliaze()
+        {
+            Config = ConfigModel.GetConfig(ConfigFileName);
         }
     }
 }
